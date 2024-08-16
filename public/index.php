87681eb010260c585e06a9a3a8cbcc728a102180
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-16 12:57:30 -0500

    Registrando URL's y los métodos asociados a ellas

--- public/index.php
+++ public/index.php
@@ -6,3 +6,8 @@ use MVC\Router;
 
 $router = new Router;
 
+$router->get('/nosotros', 'funcion_nosotros');
+$router->get('/contacto', 'funcion_contacto');
+$router->get('/admin', 'funcion_admin');
+
+$router->comprobarRutas();
