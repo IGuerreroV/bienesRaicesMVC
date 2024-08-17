c7d56172a5deb8236969dc4be62ddbb6f32f0fd7
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-16 20:10:58 -0500

    Consultar el Modelo en el Controlador y pasar Resultados hacia la vista

--- views/propiedades/admin.php
+++ views/propiedades/admin.php
@@ -1,1 +1,48 @@
-<?php echo $mensaje ?>
+<main class="contenedor seccion">
+    <h1>Administrador de Bienes Raices</h1>
+
+    <?php
+        if($resultado) {
+            $mensaje = mostrarNotificacion(intval($resultado));
+            if($mensaje) { ?>
+                <p class="alerta exito"><?php echo s($mensaje); ?></p>
+            <?php }
+        }
+    ?>
+
+    <a class="boton boton-verde" href="/admin/propiedades/crear.php">Nueva Propiedad</a>
+    <a class="boton boton-amarillo" href="/admin/vendedores/crear.php">Nuevo(a) Vendedor</a>
+
+    <h2>Propiedades</h2>
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <tbody> <!-- Mostrar los resultados -->
+            <?php foreach( $propiedades as $propiedad ):  ?>
+            <tr>
+                <td><?php echo $propiedad->id; ?></td>
+                <td><?php echo $propiedad->titulo; ?></td>
+                <td> <img class="imagen-tabla" src="/imagenes/propiedades/<?php echo $propiedad->imagen; ?>"></td>
+                <td>$ <?php echo $propiedad->precio; ?></td>
+                <td>
+                    <form class="w-100" method="POST">
+                        <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
+                        <input type="hidden" name="tipo" value="propiedad">
+                        <input type="submit" class="boton-rojo-block"  value="Eliminar">
+                    </form>
+                    
+                    <a class="boton-amarillo-block" href="admin/propiedades/actualizar.php?id=<?php echo $propiedad->id; ?>">Actualizar</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</main>
