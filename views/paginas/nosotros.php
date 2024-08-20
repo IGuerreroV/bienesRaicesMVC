d4acd97d43b192561c03a725bd2fef84bacfd83a
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-20 14:00:04 -0500

    Creando la página de Nosotros

---
+++ views/paginas/nosotros.php
@@ -0,0 +1,29 @@
+<main class="contenedor seccion">
+    <h1>Conoce sobre Nosotros</h1>
+
+    <div class="contenido-nosotros">
+        <div class="imagen">
+            <picture>
+                <source srcset="build/img/nosotros.webp" type="image/webp">
+                <source srcset="build/img/nosotros.jpg" type="image/jpeg">
+                <img loading="lazy" src="build/img/nosotros.jpg" alt="Sobre Nosotros">
+            </picture>
+        </div>
+
+        <div class="texto-nosotros">
+            <blockquote>
+                25 años de experiencia
+            </blockquote>
+
+            <p>Atque facere necessitatibus recusandae, omnis doloremque inventore vel, sunt possimus molestias magni enim rerum cupiditate pariatur. Ullam libero praesentium eaque est obcaecati.</p>
+
+            <p>Dolorum non accusantium quam, accusamus aliquid minus vitae cumque repellendus quisquam reiciendis minima quod! Exercitationem est beatae officiis quo tempore aliquam accusantium.</p>
+        </div>
+    </div>
+</main>
+
+<section class="contenedor seccion">
+    <h1>Más Sobre Nosotros</h1>
+
+    <?php include 'iconos.php' ?>
+</section>
