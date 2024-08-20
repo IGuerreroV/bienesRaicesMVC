3603e3348b23209b6f17fdae442e5819c33bd89a
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-19 20:23:28 -0500

    Creando el Controlador para Vendedores

---
+++ controllers/VendedorController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Controllers;
+
+class VendedorController {
+    public static function crear() {
+        echo 'Crear vendedor';
+    }
+
+    public static function actualizar() {
+        echo 'actualizar vendedor';
+    }
+
+    public static function eliminar() {
+        echo 'eliminar vendedor';
+    }
+}
