6848ae31c8a8bafec3109a21aff6016fecc79916
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-20 15:05:52 -0500

    Creando la página de Blog

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -37,11 +37,11 @@ class PaginasController {
             'propiedad' => $propiedad
         ]);
     }
-    public static function blog() {
-        echo 'Desde blog';
+    public static function blog(Router $router) {
+        $router->render('paginas/blog');
     }
-    public static function entrada() {
-        echo 'Desde entrada';
+    public static function entrada(Router $router) {
+        $router->render('paginas/entrada');
     }
     public static function contacto() {
         echo 'Desde contacto';
---
+++ views/paginas/entrada.php
@@ -0,0 +1,17 @@
+<main class="contenedor seccion contenido-centrado">
+    <h1>Guía para la decoración de tu hogar</h1>
+
+    <picture>
+        <source srcset="build/img/destacada2.webp" type="image/webp">
+        <source srcset="build/img/destacada2.jpg" type="image/jpeg">
+        <img loading="lazy" src="build/img/destacada2.jpg" alt="imagen de la propiedad">
+    </picture>
+
+    <p class="informacion-meta">Escrito el: <span>30/07/2024</span> por: <span>Ivan Guerrero</span></p>
+
+    <div class="resumen-propiedad">
+        <p>Vivamus congue, tortor maximus viverra scelerisque, odio libero varius nisi, quis rhoncus libero massa venenatis felis. Proin condimentum, eros quis sollicitudin tempus, dolor elit pharetra nisl, scelerisque ornare felis sapien vel turpis. Curabitur dignissim elementum consectetur. Sed tincidunt turpis vitae ante suscipit sodales. Ut molestie vulputate mauris ac dapibus. Etiam non euismod eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+        <p>Aenean ut nisl at nisi auctor cursus. Donec consequat ipsum eu nunc imperdiet, facilisis aliquet risus eleifend. In vel aliquam tellus. Phasellus iaculis dolor lorem, pretium egestas risus molestie id. Vestibulum viverra pulvinar elit, sed malesuada tortor porta at. Maecenas fringilla egestas quam at ultricies. Aenean in dui vitae ipsum ultrices tristique tristique sed odio. Vestibulum vel ullamcorper eros, sit amet molestie justo. Sed at mattis erat. Nam mi turpis, gravida a ultricies sit amet, mattis vitae magna. Nulla facilisi.</p>
+    </div>
+</main>
