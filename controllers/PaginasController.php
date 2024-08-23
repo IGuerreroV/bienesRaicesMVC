978d34f492c8dd6386ee98a0da400c8e462f317e
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-23 18:39:55 -0500

    Ocultar o Mostrar los campos del formulario según la selección del usuario

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -60,60 +60,60 @@ class PaginasController {
     }
     public static function contacto(Router $router) {
 
-        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-            $respuestas = $_POST['contacto'];
+        //     $respuestas = $_POST['contacto'];
 
-            // Crear una instacia de PHPMailer
-            $mail = new PHPMailer(true);
+        //     // Crear una instacia de PHPMailer
+        //     $mail = new PHPMailer(true);
 
-            // Configurar SMTP
-            try {
-                $mail->isSMTP();
-                $mail->Host = 'sandbox.smtp.mailtrap.io';
-                $mail->SMTPAuth = true;
-                $mail->Username = '0809a6471fc3a9';
-                $mail->Password = '6b3100de5288da';
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-                $mail->Port = 2525;
+        //     // Configurar SMTP
+        //     try {
+        //         $mail->isSMTP();
+        //         $mail->Host = 'sandbox.smtp.mailtrap.io';
+        //         $mail->SMTPAuth = true;
+        //         $mail->Username = '0809a6471fc3a9';
+        //         $mail->Password = '6b3100de5288da';
+        //         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        //         $mail->Port = 2525;
 
-                // Configurar el contenido del mail
-                $mail->setFrom('admin@bienesraices.com');
-                $mail->addAddress('admin@bienesraices.com', 'BienesRaices.com');
-                $mail->Subject = 'Tienes un Nuevo Mensaje';
+        //         // Configurar el contenido del mail
+        //         $mail->setFrom('admin@bienesraices.com');
+        //         $mail->addAddress('admin@bienesraices.com', 'BienesRaices.com');
+        //         $mail->Subject = 'Tienes un Nuevo Mensaje';
                 
-                // Habilitar HTML
-                $mail->isHTML(true);
-                $mail->CharSet = 'UTF-8';
+        //         // Habilitar HTML
+        //         $mail->isHTML(true);
+        //         $mail->CharSet = 'UTF-8';
 
-                // Definir el contenido
-                $contenido = '<html>';
-                $contenido .= '<p>Tienes un nuevo mensaje</p>';
-                $contenido .= '<p>Nombre: ' . $respuestas['nombre'] . '</p>';
-                $contenido .= '<p>Email: ' . $respuestas['email'] . '</p>';
-                $contenido .= '<p>Teléfono: ' . $respuestas['telefono'] . '</p>';
-                $contenido .= '<p>Mensaje: ' . $respuestas['mensaje'] . '</p>';
-                $contenido .= '<p>Vende o Compra: ' . $respuestas['tipo'] . '</p>';
-                $contenido .= '<p>Precio o Presupuesto: $' . $respuestas['precio'] . '</p>';
-                $contenido .= '<p>Prefiere ser contactado por: ' . $respuestas['contacto'] . '</p>';
-                $contenido .= '<p>Fecha Contacto: ' . $respuestas['fecha'] . '</p>';
-                $contenido .= '<p>Hora: ' . $respuestas['hora'] . '</p>';
-                $contenido .= '</html>';
+        //         // Definir el contenido
+        //         $contenido = '<html>';
+        //         $contenido .= '<p>Tienes un nuevo mensaje</p>';
+        //         $contenido .= '<p>Nombre: ' . $respuestas['nombre'] . '</p>';
+        //         $contenido .= '<p>Email: ' . $respuestas['email'] . '</p>';
+        //         $contenido .= '<p>Teléfono: ' . $respuestas['telefono'] . '</p>';
+        //         $contenido .= '<p>Mensaje: ' . $respuestas['mensaje'] . '</p>';
+        //         $contenido .= '<p>Vende o Compra: ' . $respuestas['tipo'] . '</p>';
+        //         $contenido .= '<p>Precio o Presupuesto: $' . $respuestas['precio'] . '</p>';
+        //         $contenido .= '<p>Prefiere ser contactado por: ' . $respuestas['contacto'] . '</p>';
+        //         $contenido .= '<p>Fecha Contacto: ' . $respuestas['fecha'] . '</p>';
+        //         $contenido .= '<p>Hora: ' . $respuestas['hora'] . '</p>';
+        //         $contenido .= '</html>';
 
-                $mail->Body = $contenido;
-                $mail->AltBody = 'Esto es texto alternativo sin HTML';
+        //         $mail->Body = $contenido;
+        //         $mail->AltBody = 'Esto es texto alternativo sin HTML';
 
-                // Habilitar la depuracion de SMTP
-                $mail->SMTPDebug = 0;
+        //         // Habilitar la depuracion de SMTP
+        //         $mail->SMTPDebug = 0;
 
-                // Enviar el email
-                $mail->send();
-                echo 'Mensaje enviado Correctamente';
-            } catch (Exception $error) {
-                // Capturar y mostrar el error
-                echo 'El mensaje no se pudo enviar. Error de Mailer: ' . $mail->ErrorInfo;
-            }
-        }
+        //         // Enviar el email
+        //         $mail->send();
+        //         echo 'Mensaje enviado Correctamente';
+        //     } catch (Exception $error) {
+        //         // Capturar y mostrar el error
+        //         echo 'El mensaje no se pudo enviar. Error de Mailer: ' . $mail->ErrorInfo;
+        //     }
+        // }
 
         $router->render('paginas/contacto', [
 
