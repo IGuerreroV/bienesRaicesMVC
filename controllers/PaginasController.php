8225b4a4d9c1ce069cbab173749ad8234d907dc3
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-23 15:49:21 -0500

    Configurando Mailtrap y enviando un email

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -5,6 +5,8 @@ namespace Controllers;
 use Model\Articulo;
 use MVC\Router;
 use Model\Propiedad;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 class PaginasController {
     public static function index(Router $router) {
@@ -59,7 +61,43 @@ class PaginasController {
     public static function contacto(Router $router) {
 
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
-            debuguear($_POST);
+            // Crear una instacia de PHPMailer
+            $mail = new PHPMailer(true);
+
+            // Configurar SMTP
+            try {
+                $mail->isSMTP();
+                $mail->Host = 'sandbox.smtp.mailtrap.io';
+                $mail->SMTPAuth = true;
+                $mail->Username = '0809a6471fc3a9';
+                $mail->Password = '6b3100de5288da';
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->Port = 2525;
+
+                // Configurar el contenido del mail
+                $mail->setFrom('admin@bienesraices.com');
+                $mail->addAddress('admin@bienesraices.com', 'BienesRaices.com');
+                $mail->Subject = 'Tienes un Nuevo Mensaje';
+                
+                // Habilitar HTML
+                $mail->isHTML(true);
+                $mail->CharSet = 'UTF-8';
+
+                // Definir el contenido
+                $contenido = '<html><p>Tienes un nuevo mensaje</p></html>';
+                $mail->Body = $contenido;
+                $mail->AltBody = 'Esto es texto alternativo sin HTML';
+
+                // Habilitar la depuracion de SMTP
+                $mail->SMTPDebug = 0;
+
+                // Enviar el email
+                $mail->send();
+                echo 'Mensaje enviado Correctamente';
+            } catch (Exception $error) {
+                // Capturar y mostrar el error
+                echo 'El mensaje no se pudo enviar. Error de Mailer: ' . $mail->ErrorInfo;
+            }
         }
 
         $router->render('paginas/contacto', [
