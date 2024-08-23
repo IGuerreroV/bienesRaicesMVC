a69fe62dc25593fc8ab74b7013dc6f6e0ae84610
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-23 16:07:15 -0500

    Enviar un Email con los datos del formulario

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -61,6 +61,9 @@ class PaginasController {
     public static function contacto(Router $router) {
 
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $respuestas = $_POST['contacto'];
+
             // Crear una instacia de PHPMailer
             $mail = new PHPMailer(true);
 
@@ -84,7 +87,19 @@ class PaginasController {
                 $mail->CharSet = 'UTF-8';
 
                 // Definir el contenido
-                $contenido = '<html><p>Tienes un nuevo mensaje</p></html>';
+                $contenido = '<html>';
+                $contenido .= '<p>Tienes un nuevo mensaje</p>';
+                $contenido .= '<p>Nombre: ' . $respuestas['nombre'] . '</p>';
+                $contenido .= '<p>Email: ' . $respuestas['email'] . '</p>';
+                $contenido .= '<p>Teléfono: ' . $respuestas['telefono'] . '</p>';
+                $contenido .= '<p>Mensaje: ' . $respuestas['mensaje'] . '</p>';
+                $contenido .= '<p>Vende o Compra: ' . $respuestas['tipo'] . '</p>';
+                $contenido .= '<p>Precio o Presupuesto: $' . $respuestas['precio'] . '</p>';
+                $contenido .= '<p>Prefiere ser contactado por: ' . $respuestas['contacto'] . '</p>';
+                $contenido .= '<p>Fecha Contacto: ' . $respuestas['fecha'] . '</p>';
+                $contenido .= '<p>Hora: ' . $respuestas['hora'] . '</p>';
+                $contenido .= '</html>';
+
                 $mail->Body = $contenido;
                 $mail->AltBody = 'Esto es texto alternativo sin HTML';
 
